101a47c22fb3cac2301752e99272bfe9b3923073
Cedric Champeau <cedric.champeau@gmail.com>
2019-03-19 14:22:58 +0100

    Initial contents

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
+import org.asciidoctor.gradle.AsciidoctorTask
+
 plugins {
     id("org.gradle.presentation.asciidoctor")
 }
 
 presentation {
     githubUserName.set("melix")
+}
+
+tasks.named<AsciidoctorTask>("asciidoctor").configure {
+    resources(delegateClosureOf<CopySpec>({
+        from("src/resources")
+    }))
 }
